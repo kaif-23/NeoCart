1ff673b28c39f9095a12325dbcae4356db044818
kaif Ali <kaifali.6263@gmail.com>
2026-02-05 00:23:58 +0530

    Update CORS origin to include admin frontend

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-   origin: ["https://neocart-frontend.onrender.com", "http://localhost:5174"],
+   origin: ["https://neocart-frontend.onrender.com", "https://neocart-admin.onrender.com"],
    credentials: true
 }))
 
