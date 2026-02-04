67d98a70e66aeb0a09eff5696612d9948654fec1
kaif ali <kaifali.6263@gmail.con>
2026-02-04 19:33:36 +0530

    feat: implement routing and user authentication for pages, add shop context and product filtering

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,14 +3,19 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import AuthContext from './context/AuthContext.jsx'
+import AuthContext from './context/authContext.jsx'
+import UserContext from './context/UserContext.jsx'
+import ShopContext from './context/ShopContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <AuthContext>
-<App />
-  </AuthContext>
-    
-  </BrowserRouter>
- 
+    <UserContext>
+      <ShopContext>
+    <App />
+    </ShopContext>
+    </UserContext>
+    </AuthContext>
+</BrowserRouter>
+  
 )
